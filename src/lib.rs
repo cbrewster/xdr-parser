0431774359f984e64bcfa628b31acc213f504398
Connor Brewster <cbrewster@hey.com>
2022-03-31 23:36:07 -0500

    Fix lifetime issues with parse error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod parser;
 
 // Parse an XDR specification. The parser supports both RFC 1831 and RFC 1832.
-pub fn parse_specification(input: &str) -> nom::IResult<&str, parser::Specification> {
-    parser::specification(input)
+pub fn parse_specification(
+    input: &str,
+) -> nom::IResult<&str, parser::Specification, nom::error::Error<String>> {
+    parser::specification::<(&str, nom::error::ErrorKind)>(input)
+        .map_err(|err| err.map(|(input, code)| nom::error::Error::new(input.to_string(), code)))
 }
